9366815cb84fdd6721ab32a0833fecc6403bcb33
Daniel <daniel.trejobanos@sdsc.ethz.ch>
2022-04-13 15:25:25 +0200

    streamline compilation of CAMx using docker

--- scripts/build_inside.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-cd /root/CAMx &&
-make COMPILER=gfortran
--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-chmod +x build_inside.sh
-docker exec -it aurora /root/CAMx/build_inside.sh
+# change to AURORA dir
+AURORA_DIR="$(builtin cd ..; pwd)"
+echo "${AURORA_DIR}"
+docker run --rm --name aurora \
+    -v "${AURORA_DIR}":/usr/src/aurora \
+    -w /usr/src/aurora/CAMx_v6.50.src.180430 \
+    gcc:7 \
+    make  COMPILER=gfortran
