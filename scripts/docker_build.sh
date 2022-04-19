9f606c7797ab7fd4dab56c867efd7224caad8d0b
Daniel <daniel.trejobanos@sdsc.ethz.ch>
2022-04-19 10:10:22 +0200

    add CAMx 6.5 and 7.10 installation in build dir

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -5,6 +5,18 @@ AURORA_DIR="$(builtin cd ..; pwd)"
 echo "${AURORA_DIR}"
 docker run --rm --name aurora \
     -v "${AURORA_DIR}":/usr/src/aurora \
-    -w /usr/src/aurora/CAMx_v6.50.src.180430 \
+    -w /usr/src/aurora/CAMx6.5/src \
     gcc:7 \
-    make  COMPILER=gfortran
+    make COMPILER=gfortran
+cd ${AURORA_DIR}/CAMx6.5/src
+make clean
+mv CAMx.v6.50.noMPI.gfortran ../build/
+
+docker run --rm --name aurora \
+    -v "${AURORA_DIR}":/usr/src/aurora \
+    -w /usr/src/aurora/CAMx7.1/src \
+    gcc:7 \
+    make COMPILER=gfortran
+cd ${AURORA_DIR}/CAMx7.1/src &&
+make clean &&
+mv CAMx.v7.10.noMPI.gfortran ../build/
