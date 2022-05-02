7a91e0191e1773992ecbbc120a584af00add0a6c
Daniel <daniel.trejobanos@sdsc.ethz.ch>
2022-05-02 10:59:28 +0200

    add test for check simulator and parameters
    We need at all moments to ensure the simulator and parameters are congruent

--- src/python/aurora_py/testbed/test_parameters.py
+++ src/python/aurora_py/testbed/test_parameters.py
@@ -6,24 +6,53 @@ from src.python.aurora_py.simulator import Simulator, SimulatorName
 
 class MockParameterAdapter(Parameters):
 
+    def __init__(self):
+        self._values = None
+        self._target_simulator = None
+
     @property
     def values(self):
-        return self.values
+        return self._values
 
     def is_correct_target(self, simulator: Simulator):
-        if self.model_name != Simulator.name:
-            return False
+        return self._target_simulator == simulator.name
 
     @property
     def target_simulator(self):
-        return self.model_name
+        return self._target_simulator
 
     @target_simulator.setter
     def target_simulator(self, model_name: SimulatorName):
         if model_name in SimulatorName:
-            self.model_name = model_name
+            self._target_simulator = model_name
         else:
             raise ValueError("Invalid model name")
+
+
+class MockSimulator(Simulator):
+
+    def __init__(self):
+        self._parameters = None
+        self._simulator_name = None
+
+    def run(self):
+        return 1
+
+    @property
+    def parameters(self):
+        return self._parameters
+
+    @parameters.setter
+    def parameters(self, parameters: Parameters):
+        self._parameters = parameters
+
+    @property
+    def name(self):
+        return self._simulator_name
+
+    @name.setter
+    def name(self, simulator_name: SimulatorName):
+        self._simulator_name = simulator_name
 
 
 @pytest.mark.parametrize("test_model_names", ["CAMx65", 1, 1232, 6.5, None])
@@ -38,3 +67,18 @@ def test_parameter_target_simulator_property(test_model_names):
     mock_adapter = MockParameterAdapter()
     mock_adapter.target_simulator = test_model_names
     assert True
+
+
+model_combinations = [(SimulatorName.CAMx65, SimulatorName.CAMx71, False),
+                      (SimulatorName.CAMx71, SimulatorName.CAMx65, False),
+                      (SimulatorName.CAMx71, SimulatorName.CAMx71, True),
+                      (SimulatorName.CAMx65, SimulatorName.CAMx65, True)]
+
+
+@pytest.mark.parametrize("test_model_parameters, test_model_simulator, expected", model_combinations)
+def test_is_correct_target(test_model_parameters, test_model_simulator,expected):
+    mock_adapter = MockParameterAdapter()
+    mock_adapter.target_simulator =  test_model_simulator
+    mock_simulator = MockSimulator()
+    mock_simulator.name = test_model_parameters
+    assert mock_adapter.is_correct_target(mock_simulator) == expected
