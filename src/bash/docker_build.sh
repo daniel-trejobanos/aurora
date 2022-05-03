8a9f38ca40b51651b36ddaf2ff42f5c0dd9e1805
Daniel Trejo Banos <daniel.trejobanos@sdsc.ethz.ch>
2022-05-03 15:52:37 +0200

    new dockerfile for non-renku image
    Having some issues building the renku image

--- src/bash/docker_build.sh
+++ src/bash/docker_build.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
 # change to AURORA dir
-AURORA_DIR="$(builtin cd ..; pwd)"
+AURORA_DIR="$(builtin cd ../..; pwd)"
 echo "${AURORA_DIR}"
 docker run --rm --name aurora \
     -v "${AURORA_DIR}":/usr/src/aurora \
     -w /usr/src/aurora/CAMx6.5/src \
-    gcc:7 \
+    aurora:no_renku \
     make COMPILER=gfortran
+patchelf --set-rpath '/usr/src/aurora/util/build/netcdf-c-4.6.1/lib/' /usr/src/aurora/CAMx6.5/src/CAMx.v6.50.noMPI.gfortran"
 cd ${AURORA_DIR}/CAMx6.5/src
 make clean
 mv CAMx.v6.50.noMPI.NCF4.gfortran ../build/
-patchelf --set-rpath '/usr/src/aurora/util/build/netcdf-c-4.6.1/lib/' CAMx.v6.50.noMPI.gfortran
 docker run --rm --name aurora \
     -v "${AURORA_DIR}":/usr/src/aurora \
     -w /usr/src/aurora/CAMx7.1/src \
-    gcc:7 \
-    make COMPILER=gfortran NCF=NCF4_C
+    aurora:no_renku \
+    "make COMPILER=gfortran NCF=NCF4_C && \
+    patchelf --set-rpath '/usr/src/aurora/util/build/netcdf-c-4.6.1/lib/' /usr/src/aurora/CAMx7.1/src/CAMx.v7.10.noMPI.NCF4.gfortran"
 cd ${AURORA_DIR}/CAMx7.1/src &&
 make clean &&
 mv CAMx.v7.10.noMPI.NFC4.gfortran ../build/
-patchelf --set-rpath '/usr/src/aurora/util/build/netcdf-c-4.6.1/lib/' CAMx.v7.10.noMPI.NCF4.gfortran
