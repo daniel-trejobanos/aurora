cf1741fe49b837f124bdd4d5e87e4ddeed8c6724
Daniel Trejo Banos <daniel.trejobanos@sdsc.ethz.ch>
2022-05-18 15:45:25 +0200

    restructure package
    Now package and setup are correctly defined

--- src/python/auroraPSI/setup.py
+++ src/python/auroraPSI/setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(
     name='auroraPSI',
     version='',
-    packages=['auroraPSI'],
+    packages=find_packages(),
     url='',
     license='',
     author='Daniel Trejo Banos',
