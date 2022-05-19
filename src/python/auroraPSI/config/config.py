3eb1348c5050ad95e68a970d7bcde13b9abcdeb5
Daniel Trejo Banos <daniel.trejobanos@sdsc.ethz.ch>
2022-05-19 07:51:17 +0200

    change path finding of config.json

--- src/python/auroraPSI/config/config.py
+++ src/python/auroraPSI/config/config.py
@@ -1,12 +1,13 @@
 from json import load
 import os
 
+import pkg_resources
+
 
 class AuroraConfiguration:
 
     def __init__(self):
-        current_dir = str(os.path.dirname(__file__))
-        self._config_path = current_dir + "/config.json"
+        self._config_path = pkg_resources.resource_filename('auroraPSI', 'config.json')
         with open(self._config_path, "r") as config_file:
             self._config_dictionary = load(config_file)
 
